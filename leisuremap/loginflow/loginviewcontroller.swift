e831c0d9188bf2cabf13747f74de1451e6e4cc9b
xtdwtqt <57191682@qq.com>
2018-07-25 17:17:34 +0800

    0725PM NO.2

--- leisuremap/loginflow/loginviewcontroller.swift
+++ leisuremap/loginflow/loginviewcontroller.swift
@@ -1,13 +1,51 @@
 //
-//  loginviewcontroller.swift
+//  loginViewController.swift
 //  leisuremap
 //
-//  Created by stu1 on 2018/7/23.
+//  Created by stu1 on 2018/7/25.
 //  Copyright © 2018年 stu1. All rights reserved.
 //
 
 import UIKit
 
-class loginviewcontroller: UIViewController {
+class loginViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var txtaccount: UITextField!
+    @IBOutlet weak var txtpassword: UITextField!
+    @IBOutlet weak var btmlogin: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//
+//
+//        
+//
+//    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField.tag==1{
+            textField.resignFirstResponder()
+            txtpassword.becomeFirstResponder()
+        }
+        if textField.tag==2{
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
