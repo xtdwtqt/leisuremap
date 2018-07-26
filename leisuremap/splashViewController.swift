58960dc2d6c66033f54d41c54785695b7068b932
xtdwtqt <57191682@qq.com>
2018-07-26 09:30:13 +0800

    0726 AM NO.1

--- leisuremap/splashViewController.swift
+++ leisuremap/splashViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class splashViewController: UIViewController {
+    
+    var appversion : String=""
+    @IBOutlet weak var lbversion: UILabel!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +25,47 @@ class splashViewController: UIViewController {
         let defaults: UserDefaults = UserDefaults.standard
         
         defaults.synchronize()
+        
+        appversion = ""+(Bundle.main.infoDictionary?["CFBundleShortVersionString"] as?String)!
+        lbversion.text=appversion
+        
+        
+        
+        let from : String = "https://score.azurewebsites.net/api/version/\(String(describing: appversion))"
+        let url=URL(string: from)!
+        
+        let request = URLRequest(url: url)
+        
+        let config=URLSessionConfiguration.default
+        let session=URLSession(configuration:config)
+        
+        let task=session.dataTask(with: request, completionHandler: {(data,response,error)in
+            
+            if( nil == error){
+                
+                let httpresponse = response as! HTTPURLResponse
+                let statusCode = httpresponse.statusCode
+                
+                
+                if(200 == statusCode){
+                    let datastring=NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                    
+                    let responsestring=String(datastring!)
+                    print(responsestring)
+                }
+                
+            }
+            else{
+                print(error)
+            }
+            
+            
+            
+            
+            
+        })
+        task.resume()
+        
     }
     
 
