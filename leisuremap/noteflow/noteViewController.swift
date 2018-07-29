80a0eaf84a5f923c54b413338686ebe74705a07f
xtdwtqt <57191682@qq.com>
2018-07-29 11:23:57 +0800

    0279 END

--- leisuremap/noteflow/noteViewController.swift
+++ leisuremap/noteflow/noteViewController.swift
@@ -16,7 +16,42 @@ class noteViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    
+  
+    @IBAction func btnPhotoClicked(_ sender: Any) {
+        
+       showActionSheet()
+        
+    }
+    
+    func showActionSheet() {
+    
+    let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+    
+    actionSheet.addAction(UIAlertAction(title: NSLocalizedString("Camera", comment:"camera"), style: .default, handler: { (alert:UIAlertAction!) -> Void in
+    self.camera()
+    }))
+    
+    actionSheet.addAction(UIAlertAction(title: NSLocalizedString("Gallery", comment:"gallery"), style: .default, handler: { (alert:UIAlertAction!) -> Void in
+    self.photoLibrary()
+    }))
+    
+    actionSheet.addAction(UIAlertAction(title: NSLocalizedString("Cancel",comment: "cancel"), style: .cancel, handler: nil))
+    
+    self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func camera()
+    {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let myPickerController = UIImagePickerController()
+            myPickerController.delegate = self ;
+            myPickerController.sourceType = .camera
+            self.present(myPickerController, animated: true, completion: nil)
+        }
+        
+    }
+    
     /*
     // MARK: - Navigation
 
